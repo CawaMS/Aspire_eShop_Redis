6f350e29e6d79506bb4f799145764e5ed8f741ad
Catherine Wang <cawa@microsoft.com>
2024-10-19 12:22:00 -0700

    merging redis code

--- Products/Endpoints/ProductEndpoints.cs
+++ Products/Endpoints/ProductEndpoints.cs
@@ -1,15 +1,24 @@
 ﻿using DataEntities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using NuGet.Protocol;
 using Products.Data;
+using StackExchange.Redis;
+using System.Text.Json;
 
 namespace Products.Endpoints;
 
 public static class ProductEndpoints
 {
-    public static void MapProductEndpoints (this IEndpointRouteBuilder routes)
+    private static readonly string CacheKeyPostFix = "Product";
+
+    public static void MapProductEndpoints (this IEndpointRouteBuilder routes, IConnectionMultiplexer connectionMultiplexer)
     {
         var group = routes.MapGroup("/api/Product");
+        IDatabase RedisDb = connectionMultiplexer.GetDatabase();
+
 
         group.MapGet("/", async (ProductDataContext db) =>
         {
@@ -18,13 +27,26 @@ public static class ProductEndpoints
         .WithName("GetAllProducts")
         .Produces<List<Product>>(StatusCodes.Status200OK);
 
-        group.MapGet("/getProductById", async  ([FromQuery]int id, ProductDataContext db) =>
+        //group.MapGet("/{id}", async  (int id, ProductDataContext db) =>
+        group.MapGet("/getProductById", async([FromQuery]int id, ProductDataContext db) =>
         {
-            return await db.Product.AsNoTracking()
-                .FirstOrDefaultAsync(model => model.Id == id)
-                is Product model
-                    ? Results.Ok(model)
-                    : Results.NotFound();
+            var ProductString = ((await RedisDb.StringGetAsync($"{id}"+$"_{CacheKeyPostFix}")));
+            if (!ProductString.Equals(RedisValue.Null))
+            {
+                Product _product = JsonSerializer.Deserialize<Product>(ProductString.ToString());
+                return Results.Ok(_product);
+            }
+            else 
+            {
+                Product product = await db.Product.AsNoTracking()
+                               .FirstOrDefaultAsync(model => model.Id == id) is Product model ? model : throw new Exception("item not found");
+
+                await RedisDb.StringSetAsync($"{id}"+$"_{CacheKeyPostFix}",JsonSerializer.Serialize(product));
+
+                return Results.Ok(model);
+            }
+
+
         })
         .WithName("GetProductById")
         .Produces<Product>(StatusCodes.Status200OK)
--- Products/Program.cs
+++ Products/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Products.Data;
 using Products.Endpoints;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,13 +13,20 @@ builder.AddServiceDefaults();
 builder.Services.AddDbContext<ProductDataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ProductContext") ?? throw new InvalidOperationException("Connection string 'ProductsContext' not found.")));
 
+// Add Redis cache
+builder.AddRedisClient("cache");
+
 // Add services to the container.
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
 
+// Get Redis connection
+var _redisConnectionMultiplexer = app.Services.GetRequiredService<IConnectionMultiplexer>();
+//IDatabase db = _redisConnectionMultiplexer.GetDatabase();
+
 // Configure the HTTP request pipeline.
-app.MapProductEndpoints();
+app.MapProductEndpoints(_redisConnectionMultiplexer);
 
 app.UseStaticFiles();
 
