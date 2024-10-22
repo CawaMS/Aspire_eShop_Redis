8d1999a88110d1c72a0d40953143d7cb0ec0c0ac
Catherine Wang <cawa@microsoft.com>
2024-10-21 19:14:38 -0700

    merging UI enhancements

--- Store/Services/ICartService.cs
+++ Store/Services/ICartService.cs
@@ -5,8 +5,8 @@ namespace Store.Services;
 public interface ICartService
 {
     Task TransferCart(string anonymousId, string userName);
-    Task AddItem(string username, int catalogItemId, decimal price, int quantity = 1);
-    Task<Cart> SetQuantities(int cartId, Dictionary<string, int> quantities);
+    Task AddItem(string userName, int productId, decimal price, int quantity = 1);
+    Task SetQuantities(string userName, int productId, int quantity);
     Task DeleteCart(string cartId);
     Task<Cart?> GetCart(string cartId);
     IAsyncEnumerable<CartItem> GetCartItems(string username);
